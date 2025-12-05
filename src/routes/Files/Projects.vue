7943e007e23f2f5cbef57f8292fb741ab8fbbb6e
maximyz3d <maximyz3d@gmail.com>
2025-12-05 13:50:10 -0500

    Add files via upload

---
+++ src/routes/Files/Projects.vue
@@ -0,0 +1,13 @@
+<template>
+	<v-row>
+		<v-col>
+			<projects-file-list />
+		</v-col>
+	</v-row>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
